d011bb39c1d80836fd7559f651de32b82029cb0d
Lars Ködderitzsch <lars@koedderitzsch.net>
2005-05-26 11:14:08 +0000

    documentation update and website style

--- CheckstylePlugin/doc/javascript/header_footer.js
+++ CheckstylePlugin/doc/javascript/header_footer.js
@@ -1,8 +1,7 @@
 function header()
 {
 document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0"><tr>');
-document.write('<td width="126"><a href="http://www.eclipse.org/"><img src="images/brandmark.gif" alt="eclipse" height="76" width="126"/></a></td>');
-document.write('<td width="400"><br/><h1 class="header">Checkstyle Plug-in</h1></td>');
+document.write('<td width="400"><br/><h1 class="header">eclipse Checkstyle Plug-in</h1></td>');
 document.write('<td><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
 document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
 document.write('</tr></table>');
