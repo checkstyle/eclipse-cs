432f3ac5c327761a6c239174eb1e51a84dda6fd6
Lars Ködderitzsch <lars@koedderitzsch.net>
2005-05-30 06:32:55 +0000

    *** empty log message ***

--- CheckstylePlugin/doc/javascript/header_footer.js
+++ CheckstylePlugin/doc/javascript/header_footer.js
@@ -1,6 +1,6 @@
 function header()
 {
-document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0"><tr>');
+document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0" style="margin-top:10px;"><tr>');
 document.write('<td width="450"><h1 class="header">Eclipse Checkstyle Plug-in</h1></td>');
 document.write('<td><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
 document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
@@ -10,6 +10,6 @@ document.write('</tr></table>');
 function footer()
 {
 document.write('<hr />');
-document.write('<p class="copyright">Copyright © 2002-2005 David Schneider, Lars Ködderitzsch. All Rights Reserved.</p>');
+document.write('<p class="copyright">Copyright © 2002-2005 David Schneider, <a href="http://www.koedderitzsch.net" target="_blank">Lars Ködderitzsch</a>. All Rights Reserved.</p>');
 }
 
