19c8444bb224b29bbee7041ea47598e68924318c
Lars Ködderitzsch <lars@koedderitzsch.net>
2005-06-07 20:07:55 +0000

    *** empty log message ***

--- CheckstylePlugin/doc/javascript/header_footer.js
+++ CheckstylePlugin/doc/javascript/header_footer.js
@@ -1,7 +1,7 @@
 function header()
 {
-document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0" style="margin-top:10px;"><tr>');
-document.write('<td width="450"><h1 class="header">Eclipse Checkstyle Plug-in</h1></td>');
+document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0"><tr>');
+document.write('<td width="450"><img src="images/plugin_header.gif"/></td>');
 document.write('<td><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
 document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
 document.write('</tr></table>');
