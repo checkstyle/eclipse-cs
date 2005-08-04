60b378376e955702ac698fecf175347de9b34abe
Lars Ködderitzsch <lars@koedderitzsch.net>
2005-08-04 09:54:44 +0000

    some docs updated

--- CheckstylePlugin/doc/javascript/header_footer.js
+++ CheckstylePlugin/doc/javascript/header_footer.js
@@ -2,8 +2,8 @@ function header()
 {
 document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0"><tr>');
 document.write('<td width="450"><img src="images/plugin_header.gif"/></td>');
-document.write('<td><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
-document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
+document.write('<td><a href="http://sourceforge.net/" target="_blank"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
+document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344" target="_blank"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
 document.write('</tr></table>');
 }
 
