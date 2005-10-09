a72997ec82cd24d35e09acdc1a080b46d5092ae7
Lars Ködderitzsch <lars@koedderitzsch.net>
2005-10-09 07:23:58 +0000

    Documentation update

--- CheckstylePlugin/doc/javascript/header_footer.js
+++ CheckstylePlugin/doc/javascript/header_footer.js
@@ -1,8 +1,8 @@
 function header()
 {
-document.write('<table class="header" width="100%" cellspacing="0" cellpadding="0" hspace="0" vspace="0"><tr>');
-document.write('<td width="450"><img src="images/plugin_header.gif"/></td>');
-document.write('<td><a href="http://sourceforge.net/" target="_blank"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
+document.write('<table class="header" width="100%" height="99" cellspacing="0" cellpadding="0" border="0"><tr>');
+document.write('<td width="450" height="99"><img src="images/plugin_header.gif"/></td>');
+document.write('<td height="99"><a href="http://sourceforge.net/" target="_blank"><img src="http://sourceforge.net/sflogo.php?group_id=80344&amp;amp;type=1" alt="SourceForge.net Logo" /></a>');
 document.write('<br/><a href="http://sourceforge.net/donate/index.php?group_id=80344" target="_blank"><img src="http://images.sourceforge.net/images/project-support.jpg" alt="Support This Project" /></a></td>');
 document.write('</tr></table>');
 }
@@ -10,6 +10,6 @@ document.write('</tr></table>');
 function footer()
 {
 document.write('<hr />');
-document.write('<p class="copyright">Copyright (c) 2002-2005 David Schneider, <a href="http://www.koedderitzsch.net" target="_blank">Lars Ködderitzsch</a>. All Rights Reserved.</p>');
+document.write('<p class="copyright">Copyright &copy; 2002-2005 David Schneider, <a href="http://www.koedderitzsch.net" target="_blank">Lars K&ouml;dderitzsch</a>. All Rights Reserved.</p>');
 }
 
