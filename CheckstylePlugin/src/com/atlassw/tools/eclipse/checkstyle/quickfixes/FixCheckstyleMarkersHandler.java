33f3915aac6e656aba096f7dc6e2b20d360ccf7e
Lars Ködderitzsch <lars@koedderitzsch.net>
2007-04-19 21:32:17 +0000

    Complete - taskAdd keybinding for Apply Checkstyle fixes action http://sourceforge.net/tracker/?group_id=80344&func=detail&atid=559497&aid=1699557

---
+++ CheckstylePlugin/src/com/atlassw/tools/eclipse/checkstyle/quickfixes/FixCheckstyleMarkersHandler.java
@@ -0,0 +1,85 @@
+//============================================================================
+//
+// Copyright (C) 2002-2007  David Schneider, Lars Ködderitzsch
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//============================================================================
+
+package com.atlassw.tools.eclipse.checkstyle.quickfixes;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+/**
+ * Command handler to enable key-binding support for the "Apply Checkstyle
+ * fixes" action.
+ * 
+ * @author Lars Ködderitzsch
+ */
+public class FixCheckstyleMarkersHandler extends AbstractHandler
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object execute(ExecutionEvent arg0) throws ExecutionException
+    {
+
+        ITextEditor editor = getActiveEditor();
+        IEditorInput input = editor.getEditorInput();
+
+        if (input instanceof FileEditorInput)
+        {
+
+            IFile file = ((FileEditorInput) input).getFile();
+
+            // call the fixing job
+            Job job = new FixCheckstyleMarkersJob(file);
+            job.setUser(true);
+            job.schedule();
+        }
+        return null;
+    }
+
+    private ITextEditor getActiveEditor()
+    {
+        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (window != null)
+        {
+            IWorkbenchPage page = window.getActivePage();
+            if (page != null)
+            {
+                IEditorPart editor = page.getActiveEditor();
+                if (editor instanceof ITextEditor)
+                {
+                    return (ITextEditor) editor;
+                }
+            }
+        }
+        return null;
+    }
+
+}
