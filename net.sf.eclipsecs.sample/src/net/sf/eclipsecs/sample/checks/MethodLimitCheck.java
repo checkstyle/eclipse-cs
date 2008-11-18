5a84069a6dc6571f2a94d222435b08df0f30fcaf
Lars Ködderitzsch <lars@koedderitzsch.net>
2008-11-18 15:43:25 +0000

    Sample project for eclipse-cs extensions

---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/MethodLimitCheck.java
@@ -0,0 +1,31 @@
+
+package net.sf.eclipsecs.sample.checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class MethodLimitCheck extends Check {
+
+    private int max = 30;
+
+    public int[] getDefaultTokens() {
+        return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+    }
+
+    public void setMax(int limit) {
+        max = limit;
+    }
+
+    public void visitToken(DetailAST ast) {
+        // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
+        DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+        // count the number of direct children of the OBJBLOCK
+        // that are METHOD_DEFS
+        int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
+        // report error if limit is reached
+        if (methodDefs > max) {
+            log(ast.getLineNo(), "too many methods, only {0} are allowed", max);
+        }
+    }
+}
---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/filter/SampleFilter.java
@@ -0,0 +1,13 @@
+
+package net.sf.eclipsecs.sample.filter;
+
+import net.sf.eclipsecs.core.projectconfig.filters.AbstractFilter;
+
+public class SampleFilter extends AbstractFilter {
+
+    public boolean accept(Object element) {
+
+        return false;
+    }
+
+}
