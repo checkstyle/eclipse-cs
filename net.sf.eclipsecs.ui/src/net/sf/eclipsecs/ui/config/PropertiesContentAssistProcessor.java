71878c3ae6a56b6a7c78656e3c792cbe427fb0ae
Lars Ködderitzsch <lars@koedderitzsch.net>
2010-02-18 21:41:28 +0000

    Upgrade copyright notice to 2010

--- net.sf.eclipsecs.ui/src/net/sf/eclipsecs/ui/config/PropertiesContentAssistProcessor.java
+++ net.sf.eclipsecs.ui/src/net/sf/eclipsecs/ui/config/PropertiesContentAssistProcessor.java
@@ -1,6 +1,6 @@
 //============================================================================
 //
-// Copyright (C) 2002-2009  David Schneider, Lars Ködderitzsch
+// Copyright (C) 2002-2010  David Schneider, Lars Ködderitzsch
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
