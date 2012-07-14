94fa48b323b4d9359a3f58532535494a28e6b4d6
Lars Ködderitzsch <lars@koedderitzsch.net>
2012-07-14 22:20:15 +0000

    Rework extension classloading

---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/MethodLimitQuickfix.java
@@ -0,0 +1,61 @@
+package net.sf.eclipsecs.sample.checks;
+
+import net.sf.eclipsecs.ui.CheckstyleUIPluginImages;
+import net.sf.eclipsecs.ui.quickfixes.AbstractASTResolution;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Javadoc;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TagElement;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.swt.graphics.Image;
+
+public class MethodLimitQuickfix extends AbstractASTResolution {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected ASTVisitor handleGetCorrectingASTVisitor(final IRegion lineInfo, final int markerStartOffset) {
+
+        return new ASTVisitor() {
+
+            public boolean visit(MethodDeclaration node) {
+
+                Javadoc doc = node.getJavadoc();
+
+                if (doc == null) {
+                    doc = node.getAST().newJavadoc();
+                    node.setJavadoc(doc);
+                }
+
+                TagElement newTag = node.getAST().newTagElement();
+                newTag.setTagName("TODO Added by MethodLimit Sample quickfix");
+
+                doc.tags().add(0, newTag);
+
+                return true;
+            }
+        };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getDescription() {
+        return "Sample MethodLimit Quickfix";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getLabel() {
+        return "Sample MethodLimit Quickfix";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Image getImage() {
+        return CheckstyleUIPluginImages.getImage(CheckstyleUIPluginImages.CORRECTION_CHANGE);
+    }
+}
