81a0161e09fb6c6f5e4b62bd04ada273e5d08569
Lars Ködderitzsch <lars@koedderitzsch.net>
2016-12-14 22:53:49 +0100

    #165 Add a button and/or a shortcut for configuration reload

---
+++ net.sf.eclipsecs.ui/src/net/sf/eclipsecs/ui/actions/PurgeCachesHandler.java
@@ -0,0 +1,42 @@
+//============================================================================
+//
+// Copyright (C) 2002-2016  David Schneider, Lars Ködderitzsch
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//============================================================================
+
+package net.sf.eclipsecs.ui.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import net.sf.eclipsecs.core.builder.CheckerFactory;
+
+/**
+ * Simple handle which clears the CheckerFactory caches in order to force reload of supplementary Checkstyle
+ * configuration files (suppressions, import control files etc.).
+ *
+ * @author Lars Ködderitzsch
+ */
+public class PurgeCachesHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        CheckerFactory.cleanup();
+        return null;
+    }
+}
