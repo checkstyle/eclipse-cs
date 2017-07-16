fc3affe6cef4cdb0e001c8b0b35ad5edfbddb4d7
Lars Ködderitzsch <lars@koedderitzsch.net>
2017-07-16 12:29:32 +0200

    Upgrade to Checkstyle 8.0

--- net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/MethodLimitCheck.java
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/MethodLimitCheck.java
@@ -6,27 +6,37 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class MethodLimitCheck extends AbstractCheck {
 
-    private int max = 30;
+  private int max = 30;
 
-    @Override
-    public int[] getDefaultTokens() {
-        return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
-    }
+  @Override
+  public int[] getAcceptableTokens() {
+    return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+  }
 
-    public void setMax(int limit) {
-        max = limit;
-    }
+  @Override
+  public int[] getRequiredTokens() {
+    return new int[0];
+  }
 
-    @Override
-    public void visitToken(DetailAST ast) {
-        // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
-        DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
-        // count the number of direct children of the OBJBLOCK
-        // that are METHOD_DEFS
-        int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
-        // report error if limit is reached
-        if (methodDefs > max) {
-            log(ast.getLineNo(), "methodlimit", max);
-        }
+  @Override
+  public int[] getDefaultTokens() {
+    return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+  }
+
+  public void setMax(int limit) {
+    max = limit;
+  }
+
+  @Override
+  public void visitToken(DetailAST ast) {
+    // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
+    DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+    // count the number of direct children of the OBJBLOCK
+    // that are METHOD_DEFS
+    int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
+    // report error if limit is reached
+    if (methodDefs > max) {
+      log(ast.getLineNo(), "methodlimit", max);
     }
+  }
 }
