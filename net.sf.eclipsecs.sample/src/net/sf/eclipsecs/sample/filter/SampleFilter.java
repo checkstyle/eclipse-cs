9825b1f60bf7170b55cf519d74a224ad5e304d2c
Michael Keppler <Michael.Keppler@gmx.de>
2022-05-28 20:41:02 +0200

    minor: add missing @Override annotations

--- net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/filter/SampleFilter.java
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/filter/SampleFilter.java
@@ -6,6 +6,7 @@ import net.sf.eclipsecs.core.projectconfig.filters.AbstractFilter;
 @ThreadSafe
 public class SampleFilter extends AbstractFilter {
 
+  @Override
   public boolean accept(Object element) {
 
     return false;
