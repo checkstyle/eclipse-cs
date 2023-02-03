783ad3c3b27c5c3956de788075d2c59366412767
rnveach <rveach02@gmail.com>
2023-02-02 23:27:56 -0500

    Issue #454: enforces header on all files and upgrades year

--- net.sf.eclipsecs.core/src/net/sf/eclipsecs/core/transformer/CTransformationClass.java
+++ net.sf.eclipsecs.core/src/net/sf/eclipsecs/core/transformer/CTransformationClass.java
@@ -1,6 +1,6 @@
 //============================================================================
 //
-// Copyright (C) 2009 Lukas Frena
+// Copyright (C) 2003-2023  Lukas Frena
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
