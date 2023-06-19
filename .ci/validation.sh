931c9ab9862bf47e4fc706d79900c30ec6e07a73
Michael Keppler <Michael.Keppler@gmx.de>
2023-06-19 19:21:42 +0200

    Ignore sitemap changes again
    Completely removing the special handling of the sitemap timestamps was
    wrong. While the other changes in the build avoided that timestamps
    change with _every_ build, they will still change after real
    documentation updates (like version number changes).
    
    Therefore re-introduce the special handling of the sitemaps file.

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -4,13 +4,20 @@ set -e
 case $1 in
 
 git-diff)
+  printf "Changes to only docs/sitemap.xml:\n"
+  git diff docs/sitemap.xml
+
+  # file below is always modified on a run and updated with the current date
+  # such a change is ignored and must be verified manually instead
+  git checkout HEAD -- docs/sitemap.xml
+
   if [ "$(git status | grep 'Changes not staged\|Untracked files')" ]; then
     printf "Please clean up.\nGit status output:\n"
     printf "Top 300 lines of diff:\n"
     git status
     git diff | head -n 300
     false
-  fi
+  fi 
   ;;
 
 *)
