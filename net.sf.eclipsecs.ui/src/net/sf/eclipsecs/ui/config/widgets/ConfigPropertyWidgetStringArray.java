88ae9a98d3e1dc6ebaf88b5770a51062d4826422
Michael Keppler <Michael.Keppler@gmx.de>
2023-09-09 16:41:40 +0200

    normalize array properties
    Ensure that array properties and multi value properties are stored with
    a normalized ", " separator (comma plus blank) in the config file,
    independent of how they have been edited in the UI.
    
    That improves readability and comparison of configuration files in the
    long run.

---
+++ net.sf.eclipsecs.ui/src/net/sf/eclipsecs/ui/config/widgets/ConfigPropertyWidgetStringArray.java
@@ -0,0 +1,59 @@
+//============================================================================
+//
+// Copyright (C) 2003-2023 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+//============================================================================
+
+package net.sf.eclipsecs.ui.config.widgets;
+
+import net.sf.eclipsecs.core.config.ConfigProperty;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Property configuration widget for string arrays.
+ */
+public class ConfigPropertyWidgetStringArray extends ConfigPropertyWidgetString {
+
+  public ConfigPropertyWidgetStringArray(Composite parent, ConfigProperty prop) {
+    super(parent, prop);
+  }
+
+  @Override
+  protected String getInitValue() {
+    return normalizeSeparator(super.getInitValue());
+  }
+
+  @Override
+  public String getValue() {
+    return normalizeSeparator(super.getValue());
+  }
+
+  /**
+   * normalize array properties to be separated by a comma and a blank for better readability of the plain config file
+   * @param text
+   * @return text with normalized separators
+   */
+  private String normalizeSeparator(String text) {
+    return Arrays.stream(text.split(","))
+            .map(String::strip)
+            .collect(Collectors.joining(", "));
+  }
+}
